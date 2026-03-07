669b934d5c1d7314ea50c8f9d0a849ec1ff3ea71
mitesh <miteshchavda57@gmail.com>
2026-03-07 18:17:11 +0530

    Up: server-cmds.sh > Fix syntax error

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,10 +1,9 @@
-#!/user/bin/env bash
-
+#!/usr/bin/env bash
 export IMAGE_NAME=$1
 
 # docker login
-export DOCKER_PASSWORD = $2
-export DOCKER_USERNAME = $3
+export DOCKER_USERNAME=$2
+export DOCKER_PASSWORD=$3
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 
 docker-compose -f docker-compose.yaml up --detach   
